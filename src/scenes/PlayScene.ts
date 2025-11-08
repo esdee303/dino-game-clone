31da087ef1ff7343ffbd036458aefbb2388cf5a9
esdee303 <steven.deferme@gmail.com>
2025-11-09 00:03:12 +0100

    S03E83 Add obstacle speed

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -11,7 +11,8 @@ class PlayScene extends GameScene {
   startTrigger: SpriteWithDynamicBody;
   spawnInterval: number = 1500;
   spawnTime: number = 0;
-  
+  obstacleSpeed: number = 10;
+
   constructor() {
     super('PlayScene');
   }
@@ -49,11 +50,15 @@ class PlayScene extends GameScene {
   }
 
   update(time: number, delta: number): void {
+    if (!this.isGameRunning) return;
+
     this.spawnTime += delta;
     if (this.spawnTime > this.spawnInterval) {
       this.spawnObstacle();
       this.spawnTime = 0;
     }
+
+    Phaser.Actions.IncX(this.obstacles.getChildren(), -this.obstacleSpeed);
   }
 
   createEnvironment() {
