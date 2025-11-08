b8a607a9f04df8cc13745ed08dfd65d7a916e923
esdee303 <steven.deferme@gmail.com>
2025-11-08 23:55:30 +0100

    S03E81 Spawn obstacles

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -4,9 +4,9 @@ import { Player } from '../entities/Player';
 import { GameScene } from './GameScene';
 
 class PlayScene extends GameScene {
-
   player: Player;
   ground: Phaser.GameObjects.TileSprite;
+  obstacles: Phaser.Physics.Arcade.Group;
   startTrigger: SpriteWithDynamicBody;
   spawnInterval: number = 1500;
   spawnTime: number = 0;
@@ -18,6 +18,7 @@ class PlayScene extends GameScene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+    this.obstacles = this.physics.add.group();
 
     this.startTrigger = this.physics.add.sprite(0, 10, null).setOrigin(0, 1).setAlpha(0);
 
@@ -26,41 +27,46 @@ class PlayScene extends GameScene {
         this.startTrigger.body.reset(0, this.gameHeight);
         return;
       }
-      this.startTrigger.body.reset(99999, 99999)
-      
+      this.startTrigger.body.reset(99999, 99999);
+
       const rollOutEvent = this.time.addEvent({
         delay: 1000 / 60,
         loop: true,
         callback: () => {
           this.player.playRunAnimation();
           this.player.setVelocityX(80);
-          this.ground.width += (17 * 2);
+          this.ground.width += 17 * 2;
           if (this.ground.width >= this.gameWidth) {
             rollOutEvent.remove();
             this.ground.width = this.gameWidth;
             this.player.setVelocityX(0);
             this.isGameRunning = true;
           }
-        }
-      })
-        
-    })
+        },
+      });
+    });
+  }
+
+  update(time: number, delta: number): void {
+    this.spawnTime += delta;
+    if (this.spawnTime > this.spawnInterval) {
+      this.spawnObstacle();
+      this.spawnTime = 0;
+    }
   }
 
   createEnvironment() {
-    this.ground = this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1)
+    this.ground = this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1);
   }
 
   createPlayer() {
     this.player = new Player(this, 0, this.gameHeight);
   }
 
-  update(time: number, delta: number): void {
-    this.spawnTime += delta;
-    if (this.spawnTime > this.spawnInterval) {
-      console.warn('Spawning obstacle')
-      this.spawnTime = 0;
-    }
+  spawnObstacle() {
+    const obstacleNumber = Math.floor(Math.random() * 6) + 1;
+    const distance = Phaser.Math.Between(600, 900);
+    this.obstacles.create(distance, this.gameHeight, `obstacle-${obstacleNumber}`).setOrigin(0, 1);
   }
 }
 
